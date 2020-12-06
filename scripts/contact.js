43df30a2f714c5cf7c3624d1bd4075595a316ca3
MinPt <petr.minailyuk@gmail.com>
2020-12-07 01:35:13 +0200

    pop-up refactor

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,8 +1,10 @@
 let contact_block = document.getElementById("contact-block");
 let contact_img = document.getElementById("contact_img");
+let pop_up_form = document.getElementById("pop-up-form");
 
 contact_block.addEventListener("click", hideContactBlock);
 contact_img.addEventListener("click", displayContactBlock);
+pop_up_form.addEventListener("click",displayContactBlock);
 
 function displayContactBlock(){
   contact_block.classList.add("active");
